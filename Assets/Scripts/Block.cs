5ee7bf96ad74389c5ca6f5d9124e6ae6066c223d
Halil Coşgun <cosgun.halil@gmail.com>
2023-04-04 12:04:17 +0300

    Add improvement notes into the block script

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collisionInfo) {
+        // TODO: eğer blocklar yıkılıp tekrar oluşturuluyorsa, bir ObjectPool'a atılabilir destroy etmek yerine.
         // Destroy the whole Block
         Destroy(gameObject);
     }
