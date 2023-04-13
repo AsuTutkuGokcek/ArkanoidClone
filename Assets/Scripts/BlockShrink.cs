7fad56e49150e017a7bc679272c7bfe07d18904a
Asu Tutku Gökçek <agokcek19@ku.edu.tr>
2023-04-13 14:36:25 +0300

    Shrink effect added on pink_block

--- Assets/Scripts/BlockShrink.cs
+++ Assets/Scripts/BlockShrink.cs
@@ -5,16 +5,18 @@ using DG.Tweening;
 
 public class BlockShrink : MonoBehaviour
 {
-    public Animation anim;
 
     void Start(){
-        anim = GetComponent<Animation>();
+
     }
     
     void OnCollisionEnter2D(Collision2D collisionInfo) {
-        // TODO: eğer blocklar yıkılıp tekrar oluşturuluyorsa, bir ObjectPool'a atılabilir destroy etmek yerine.
-        // Destroy the whole Block
-        anim.Play();
-        Destroy(gameObject, 1f);
+        transform.DOScale(new Vector3 (0,0,0), 2f);
+        StartCoroutine(SelfDestruct());
+    }
+
+        IEnumerator SelfDestruct(){
+        yield return new WaitForSeconds(2f);
+        Destroy (gameObject);
     }
 }
